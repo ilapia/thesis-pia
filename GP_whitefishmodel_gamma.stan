c48f15b28b7950ebef12ac69774031a77d7bf6a1
Pia Ilaria <piailari@lx7-500-053.science.helsinki.fi>
2020-07-10 01:11:52 +0300

    upload files

---
+++ GP_whitefishmodel_gamma.stan
@@ -0,0 +1,76 @@
+ data {
+    int<lower=1> N;
+    int<lower=1> Dx;
+    matrix[N,Dx] x;
+    int<lower=1> Ds;
+    matrix[N,Ds] s;
+    int<lower=0> y[N];
+    vector[N] V;
+  }
+  transformed data {
+    vector[N] mu;
+    matrix[N, N] Dist_spatial;
+    matrix[N, N] Sigma_lin;
+    real s2_lin;
+    s2_lin = 10;
+    for (i in 1:N)
+      mu[i] = 0;
+    // off-diagonal elements
+    for (i in 1:(N-1)) {
+      for (j in (i+1):N) {
+        Dist_spatial[i, j] = pow(dot_self(s[i] - s[j]),0.5)  ;    
+        Sigma_lin[i, j] = s2_lin * dot_product(x[i],x[j]);     // linear covariance function
+        
+        // Fill in the other half
+        Dist_spatial[j, i] = Dist_spatial[i, j];
+        Sigma_lin[j, i] = Sigma_lin[i, j];
+      }
+    }
+    // diagonal elements
+    for (k in 1:N){
+      Dist_spatial[k, k] = 0;
+      Sigma_lin[k, k] = s2_lin * dot_product(x[k],x[k]) + 1e-6;   // add also some jitter
+    }
+  }
+  parameters {
+    real<lower=0> l;
+    real<lower=0> s2_matern;
+    vector[N] z;
+    real<lower=0> r;
+  }
+  transformed parameters {
+    matrix[N, N] Sigma;
+    matrix[N, N] L;
+    real<lower=0> inv_l;
+  
+    inv_l = inv(l);
+  Sigma = s2_matern*exp(-inv_l*Dist_spatial ) + Sigma_lin; // Exponential   
+   //     Sigma = s2_matern*(1 + pow(3,0.5)*inv_l*Dist_spatial).*exp(-pow(3,0.5)*inv_l*Dist_spatial)  + Sigma_lin ;  //Matern
+    L = cholesky_decompose(Sigma);
+  }
+  model {
+    vector[N] ff;
+    
+       // A weakly informative prior for magnitude
+    s2_matern ~ student_t(4, 0, 1);
+  
+    // A weakly informative prior for l, that shrinks to 0 cor. among locations with more than 50km dist
+    l ~ gamma( 10,1);
+  
+// A weakly informative prior for overdispersion
+    r ~ gamma(2, 0.1 );
+    
+    z ~ normal(0, 1);
+    ff = L*z;
+  
+    for (n in 1:N) {
+      y[n] ~ neg_binomial_2(V[n]*exp(ff[n]), r);
+    }
+      
+    
+  }
+  generated quantities {
+    vector[N] f;
+    // derived quantity (transform)
+    f = L*z;
+  }
---
+++ Matern32Covariance.cpp
@@ -0,0 +1,61 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+NumericMatrix Matern32Covariance(NumericMatrix X1, NumericMatrix X2, double l, double sigma2, int trainingC) {
+  // C = GaussianCovariance(x1,x2,l,sigma2,training)
+  // 
+  // A function that takes matrices x1 and x2 of input locations, lengthscale
+  // parameter l and magnitude parameter sigma2 and constructs a covariance
+  // matrix between f(x1) and f(x2) corresponding to a Gaussian covariance
+  // function. 
+  //
+  // Parameters are
+  //  X1 : matrix of coordinates, n1 x d
+  //  X2 : matrix of coordinates, n2 x d
+  //  l : lengthscale
+  //  sigma2 : variance parameter
+  //  trainingC : an indicator telling whether we calculate the 
+  //              training covariance matrix (1), that is X1 == X2
+  //              or a generic matrix of covariances (0), that is X1 != X2
+  
+  // test that 
+  if (X1.ncol() != X2.ncol()){
+    throw std::range_error("number of columns in X1 and X2 do not match");
+  }
+  
+  NumericMatrix C = NumericMatrix(X1.nrow(),X2.nrow());
+  double d;
+  if (trainingC == 1){
+    // symmetric covariance matrix (training covariance)
+    for(int ii = 0; ii < C.nrow(); ii++){
+      for(int jj = ii+1; jj < C.ncol(); jj++){
+        d = sqrt(3*sum(pow(X1.row(ii)-X2.row(jj),2))/pow(l,2));
+        C(ii,jj) = sigma2*(1+d)*exp(-d);
+        C(jj,ii) = sigma2*(1+d)*exp(-d);
+      }
+      C(ii,ii) = sigma2;
+    }
+  }
+  else{
+    // nonsymmetric matrix of covariances
+    for(int ii = 0; ii < C.nrow(); ii++){
+      for(int jj = 0; jj < C.ncol(); jj++){
+        d = sqrt(3*sum(pow(X1.row(ii)-X2.row(jj),2))/pow(l,2));
+        C(ii,jj) = sigma2*(1+d)*exp(-d);
+      }
+    }
+  }
+  return C;
+}
+
